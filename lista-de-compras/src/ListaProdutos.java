8f23f4de075deeb6b06706ce893188fa8dd2e838
alex brito <alex.teixeira.brito@gmail.com>
2022-10-22 19:39:56 -0300

    Criação e teste da classe lista de compras

---
+++ lista-de-compras/src/ListaProdutos.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ListaProdutos {
+	
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.print("Digite seu item: ");
+		String produto = entrada.nextLine();
+		System.out.println(produto);
+		
+	}
+
+}
