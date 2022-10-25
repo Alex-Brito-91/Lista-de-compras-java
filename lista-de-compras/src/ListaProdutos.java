f94ff3e6ae6e8a28feb9cef495ea931b8f481a11
alex brito <alex.teixeira.brito@gmail.com>
2022-10-24 21:57:14 -0300

    Transformado em classe

--- lista-de-compras/src/ListaProdutos.java
+++ lista-de-compras/src/ListaProdutos.java
@@ -1,23 +1,10 @@
-import java.util.Scanner;
+import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class ListaProdutos {
 	
-	public static void main(String[] args) {
-		
-		Scanner entrada = new Scanner(System.in);
-		
-		ArrayList<String> listaProdutos = new ArrayList<String>();
-		
-		System.out.print("Digite seu item: ");
-		
-		listaProdutos.add(entrada.nextLine());
-		
-		Arrays.toString(listaProdutos.toArray());
-		
-		System.out.println(Arrays.toString(listaProdutos.toArray()));
-		
-	}
-
+	ArrayList<String> produto;
+	ArrayList<BigDecimal> preco;
+	ArrayList<Integer> quantidade;
+	
 }
