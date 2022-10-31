c7053c7562d441324c6e7250fe467093e323a199
alex brito <alex.teixeira.brito@gmail.com>
2022-10-31 19:56:43 -0300

    Configurando métodos para dentro da classe

--- lista-de-compras/src/Produto.java
+++ lista-de-compras/src/Produto.java
@@ -1,25 +1,62 @@
-import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Produto {
 
 	int numero;
 	String nome;
-	BigDecimal preco;
+	double preco;
 	int quantidade;
+	String opcao = "";
 
-	public Produto(int numero, String nome, BigDecimal preco, int quantidade) {
+	public static List<Produto> produtos = new ArrayList<>();
 
-		this.numero = numero;
-		this.nome = nome;
-		this.preco = preco;
-		this.quantidade = quantidade;
+	public Produto(String nome, double preco, int quantidade) {
+		
+	}
+
+
+	public void cadastra() {
+
+		while (!"N".equalsIgnoreCase(opcao)) {
+
+			Scanner scanner = new Scanner(System.in);
+
+			System.out.print("Digite o nome do produto: ");
+			nome = scanner.next();
+
+			System.out.print("Digite o valor: ");
+			preco = scanner.nextDouble();
+
+			System.out.print("Digite a quantidade: ");
+			quantidade = scanner.nextInt();
+
+			Produto produto = new Produto(nome, preco, quantidade);
+			produtos.add(produto);
+
+			System.out.println("Deseja continuar cadastrando? Digite [S] para continuar ou [N] para encerrar");
+			opcao = scanner.next();
+
+		}
+		
+		for (Produto i : produtos) {
+			System.out.println(i);
+		}
 
 	}
+	
+
+//	public void imprime(List<Produto> produtos) {
+//		for (Produto i : produtos) {
+//			System.out.println(i);
+//		}
+//	}
 
 	@Override
 	public String toString() {
-		return "|   Produto nº: [" + numero + "]   Nome: " + nome + "     |   Preço: " + preco + "     |   Quant: " + quantidade + "     |";
+		return "|   Produto nº: [" + numero + "]   Nome: " + nome + "     |   Preço: " + preco + "     |   Quant: "
+				+ quantidade + "     |";
 	}
-
 
 }
--- lista-de-compras/src/TesteProduto.java
+++ lista-de-compras/src/TesteProduto.java
@@ -1,76 +1,45 @@
-import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class TesteProduto {
 
 	public static void main(String[] args) {
+		
+		Produto produto = new Produto(null, 0, 0);
+		List<Produto> listaProdutos = new ArrayList<>();
+		
+		
+		
+		
+		
 
-		List<Produto> produtos = new ArrayList<>();
-		Scanner entrada = new Scanner(System.in);
-
-		String exit = "";
-		String limpar = "";
-		int contador = 0;
-
-		//Cadastrar itens na lista
-		while (!"N".equalsIgnoreCase(exit)) {
-
-			int numero = contador;
-			numero++;
-
-			System.out.print("Digite o nome do Produto: ");
-			String nome = entrada.next();
-
-			System.out.print("Digite o Preço: ");
-			BigDecimal preco = entrada.nextBigDecimal();
-
-			System.out.print("Digite a quantidade: ");
-			int quantidade = entrada.nextInt();
-
-			Produto lista = new Produto(numero, nome, preco, quantidade);
-			produtos.add(lista);
-			
-			contador = numero;
-
-			System.out.println("Deseja continuar cadastrando? Digite: [S] para continuar e [N] para encerrar");
-			exit = entrada.next();
-			
-			//Exibir a lista
-			for (Produto s : produtos) {
-				System.out.println(s);
-
-			}
-
-		}
 
 		
 
-		//Excluir um item da lista
-		System.out.print("Digite o número do item que deseja excluir: ");
-		int excluir = entrada.nextInt();
-		produtos.remove(excluir - 1);
-		System.out.println("removido com sucesso");
-		//Exibir a lista
-		for (Produto s : produtos) {
-			System.out.println(s);
-
-		}
+//		//Excluir um item da lista
+//		System.out.print("Digite o número do item que deseja excluir: ");
+//		int excluir = entrada.nextInt();
+//		produtos.remove(excluir - 1);
+//		System.out.println("removido com sucesso");
+//		//Exibir a lista
+//		for (Produto s : produtos) {
+//			System.out.println(s);
+//
+//		}
 		
 
-		//Limpar toda a lista
-		System.out.println("Deseja limpar a lista? Digite: [S] para continuar e [N] para cancelar");
-		limpar = entrada.next();
-		if ("S".equalsIgnoreCase(limpar)) {
-			produtos.removeAll(produtos);
-			//Exibir a lista
-			for (Produto s : produtos) {
-				System.out.println(s);
-
-			}
-
-		}
+//		//Limpar toda a lista
+//		System.out.println("Deseja limpar a lista? Digite: [S] para continuar e [N] para cancelar");
+//		limpar = entrada.next();
+//		if ("S".equalsIgnoreCase(limpar)) {
+//			produtos.removeAll(produtos);
+//			//Exibir a lista
+//			for (Produto s : produtos) {
+//				System.out.println(s);
+//
+//			}
+//
+//		}
 
 	}
 
