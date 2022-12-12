9204f1e93add960d9dafa014ccb52f799edcbc37
alex brito <alex.teixeira.brito@gmail.com>
2022-12-12 16:48:16 -0300

    Ajustes na classe da tela de cadastro

--- lista-de-compras/src/TelaCadastro.java
+++ lista-de-compras/src/TelaCadastro.java
@@ -1,23 +1,27 @@
 
 import java.awt.EventQueue;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
+import javax.swing.SpinnerModel;
+import javax.swing.SpinnerNumberModel;
 import javax.swing.JButton;
-import java.awt.event.ActionListener;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
-import java.awt.event.ActionEvent;
+import javax.swing.JSpinner;
 
-public class TelaCompras1 {
+public class TelaCadastro {
 
-	private JFrame frame;
+	private JFrame frameCadastro;
 	private JTextField textProduto;
 	private JTextField textValorUn;
 	private JLabel lblQuantidade;
-	private JTextField textQuantidade;
+	private JSpinner spinner;
 
 	public static List<Produto> produtos = new ArrayList<>();
 
@@ -39,8 +43,8 @@ public class TelaCompras1 {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					TelaCompras1 window = new TelaCompras1();
-					window.frame.setVisible(true);
+					TelaCadastro window = new TelaCadastro();
+					window.frameCadastro.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -48,59 +52,76 @@ public class TelaCompras1 {
 		});
 	}
 
-	public TelaCompras1() {
+	public TelaCadastro() {
 		initialize();
 	}
 
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
+		frameCadastro = new JFrame();
+		frameCadastro.setBounds(100, 100, 450, 300);
+		frameCadastro.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frameCadastro.getContentPane().setLayout(null);
 
 		JLabel lblProduto = new JLabel("Produto:");
-		lblProduto.setBounds(53, 24, 70, 15);
-		frame.getContentPane().add(lblProduto);
+		lblProduto.setBounds(48, 24, 70, 15);
+		frameCadastro.getContentPane().add(lblProduto);
 
 		textProduto = new JTextField();
-		textProduto.setBounds(127, 22, 114, 19);
-		frame.getContentPane().add(textProduto);
+		textProduto.setBounds(112, 19, 114, 25);
+		frameCadastro.getContentPane().add(textProduto);
 		textProduto.setColumns(10);
 
 		JLabel lblValorUnitrio = new JLabel("Valor Unitário:");
-		lblValorUnitrio.setBounds(13, 58, 110, 15);
-		frame.getContentPane().add(lblValorUnitrio);
+		lblValorUnitrio.setBounds(7, 58, 110, 15);
+		frameCadastro.getContentPane().add(lblValorUnitrio);
 
 		textValorUn = new JTextField();
-		textValorUn.setBounds(127, 57, 114, 19);
-		frame.getContentPane().add(textValorUn);
+		textValorUn.setBounds(112, 51, 114, 25);
+		frameCadastro.getContentPane().add(textValorUn);
 		textValorUn.setColumns(10);
 
 		lblQuantidade = new JLabel("Quantidade:");
-		lblQuantidade.setBounds(28, 90, 90, 15);
-		frame.getContentPane().add(lblQuantidade);
-
-		textQuantidade = new JTextField();
-		textQuantidade.setBounds(127, 90, 114, 19);
-		frame.getContentPane().add(textQuantidade);
-		textQuantidade.setColumns(10);
+		lblQuantidade.setBounds(23, 90, 90, 15);
+		frameCadastro.getContentPane().add(lblQuantidade);
+		
+		SpinnerModel sm = new SpinnerNumberModel(1,1,20,1);
+		spinner = new JSpinner(sm);
+		spinner.setBounds(112, 84, 41, 26);
+		frameCadastro.getContentPane().add(spinner);
 
 		JButton btnCadastrar = new JButton("Cadastrar");
 		btnCadastrar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-					
+
 				int geraCodigo = 0;
 				Produto p = new Produto();
-				
+
+				p.setId(++geraCodigo);
+
 				String nome = textProduto.getText();
-				Integer quantidade = Integer.parseInt(textQuantidade.getText());
+				Integer quantidade = Integer.parseInt(spinner.getValue().toString());
 				BigDecimal valorUnitario = new BigDecimal(textValorUn.getText());
 				BigDecimal valorTotal = valorUnitario.multiply(BigDecimal.valueOf(quantidade));
-				
+
+				p.setNome(nome);
+				p.setQuantidade(quantidade);
+				p.setValorUnitario(valorUnitario);
+				p.setValorTotal(valorTotal);
+
+				produtos.add(p);
+
 				JOptionPane.showMessageDialog(null, "Produto " + textProduto.getText() + " cadastrado com sucesso!");
 
 			}
-			
-		});btnCadastrar.setBounds(300,22,117,25);frame.getContentPane().add(btnCadastrar);
-}
+
+		});
+		btnCadastrar.setBounds(300, 22, 117, 25);
+		frameCadastro.getContentPane().add(btnCadastrar);
+		
+		JButton btnListaButton = new JButton("exibir lista");
+		
+		btnListaButton.setBounds(162, 196, 125, 25);
+		frameCadastro.getContentPane().add(btnListaButton);
+
+	}
 }
